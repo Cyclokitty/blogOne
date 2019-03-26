517a2a005e7b166e6a4d9f45e4363f53c7bd533e
Cyclokitty <laura@vingada.com>
2019-03-26 18:07:25 -0400

    Updates

---
+++ precache-manifest.0ed5fe6851701c4dcfe734c739c8799b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98baca5f4b34c8fd3c0c",
+    "url": "/blogOne/static/css/main.942db692.chunk.css"
+  },
+  {
+    "revision": "98baca5f4b34c8fd3c0c",
+    "url": "/blogOne/static/js/main.0fab963d.chunk.js"
+  },
+  {
+    "revision": "376100779c360a06deaf",
+    "url": "/blogOne/static/js/runtime~main.bc6cc621.js"
+  },
+  {
+    "revision": "dc85ff650925a37e08c0",
+    "url": "/blogOne/static/js/2.97fa57ce.chunk.js"
+  },
+  {
+    "revision": "a9fe53825b1b6fb28c7ffeea6796d4bf",
+    "url": "/blogOne/index.html"
+  }
+];
